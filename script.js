20d57ab07aa8d72ad5fb561add977bf9a87763b7
Jasmine Kay <jasminekay505@gmail.com>
2021-02-04 19:35:01 -0800

    Adding function to get weather data added link to moment to get current date

--- script.js
+++ script.js
@@ -10,9 +10,36 @@ $("#search").on("click", function (event) {
     event.preventDefault();
 
     cityName = $("#input").val().trim();
-    console.log = cityName;
-    //cityList.push(cityName);
-    //console.log(cityList);
+    if (cityName != "") { 
+        console.log(cityName);
+        cityList.push(cityName);
+        console.log(cityList);
+    } else { 
+        alert("Please enter a city!");
+    }
+    getWeatherData();
+    
 })
+
+//function to get and display weather
+function getWeatherData() {
+        var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=0223917225e2fc61b70b2c4dc9942a0d";
+
+        fetch(requestURL)
+        .then(function(response) { 
+            return response.json();
+        })
+        .then(function(data) {
+            var currentWeather = $("<div>")
+            $(currentWeather).addClass("card-body");
+            $(currentWeather).attr("id", "current-weather");
+            
+            console.log(data);
+        })
+    
+}
+
+getWeatherData();
+
 
 });
